a8cb8ce5d4aa9d391522977ab835fd87a62da840
lead <lead@mouthmelt.com>
2020-12-17 16:22:26 +0800

    添加ansible更改密码

--- centos7-env-deploy/common/changepwd.bat
+++ centos7-env-deploy/common/changepwd.bat
@@ -1,6 +1,6 @@
+echo "success" 2>"C:\Users\hz\Desktop\"chpwd_at_%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%.log"
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d Administrator /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "x5$EHoO?UKZzrM.W" /f
 net user Administrator "x5$EHoO?UKZzrM.W"
-
 vsfZUGK^x4eFC7gi
