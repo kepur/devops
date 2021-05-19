bbd0fca05f8e373c03f1a5a2d128c358b50926d8
hozin <hozin@gmail.com>
2021-05-19 22:44:52 +0800

    add redis installs

--- centos7-env-deploy/centos7_redis_install.sh
+++ centos7-env-deploy/centos7_redis_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-redisurl="https://linux-1254084810.cos.ap-chengdu.myqcloud.com/redis-4.0.6.tar.gz"
-redis="https://download.redis.io/releases/redis-6.2.3.tar.gz"
-
 redis4.0.6_install(){
 	echo "正在执行redis安装"
 	sleep 1s
@@ -31,39 +28,80 @@ redis4.0.6_install(){
 	systemctl enable redis.service
 	systemctl restart redis.service
 }
-redis5.0_install(){
-	tar -zxvf redis-4.0.6.tar.gz
-	cd /opt/redis-4.0.6
+
+#https://download.redis.io/releases/redis-6.2.3.tar.gz?_ga=2.171752800.1114973999.1621433299-296576591.1621433299
+redis_root_url="https://download.redis.io/releases/"
+pkg_dir=/opt/pkg_dir
+redis_install(){
+        redis_version=$1
+	echo $redis_version
+	Redis=redis-$redis_version.tar.gz
+	echo "$python"
+	if [ -f "$pkg_dir$Redis" ];then
+		echo " 文件 $Redis 找到 "
+	else
+		echo "文件 $Redis 不存在将自动下载" 
+		if ! wget -c -t3 -T60 ${redis_root_url}/$redis_version/$Redis -P $pkg_dir/; then
+            echo "Failed to download $Redis \n 下载$Redis失败, 请手动下载到${pkg_dir} \n please download it to ${pkg_dir} directory manually and try again."
+            echo -e "请把下列安装包放到$pkg_dir目录下 \n\n " $$ sleep 2s
+			exit 1
+        fi
+	fi
+    cd $pkg_dir && echo "正在执行Redis安装"
+	tar -zxvf $Redis
+	cd redis-$redis_version\
 	make MALLOC=libc
-	cd /opt/redis-4.0.6/src
+	cd \src
 	make install 
-	mkdir -p /etc/redis
-	cp /opt/redis-4.0.6/redis.conf /etc/redis/6379.conf
-	cp /opt/redis-4.0.6/utils/redis_init_script /etc/init.d/redisd
-	echo "net.core.somaxconn = 2048" >> /etc/sysctl.conf 
-	echo 511 > /proc/sys/net/core/somaxconn
-	echo 2 > /proc/sys/vm/overcommit_memory
-	echo 60 > /proc/sys/vm/overcommit_ratio
-	echo never > /sys/kernel/mm/transparent_hugepage/enabled
-	sysctl -p
+	cp $pkg_dir/redis-$redis_version/redis.conf /etc/redis/6379.conf
+	cp $pkg_dir/redis-$redis_version/utils/redis_init_script /etc/init.d/redisd
 	sed -i '2i\
 	# chkconfig:   2345 90 10
 	# description:  Redis is a persistent key-value database' /etc/init.d/redisd
 	echo '''
 	[Unit]
-	Description=The redis-server Process Manager
-	After=syslog.target network.target
+	Description=redis-server
+	After=network.target
 
 	[Service]
-	Type=simple
-	PIDFile=/var/run/redis_6379.pid
-	ExecStart=/usr/local/bin/redis-server /etc/redis/6379.conf 
-	ExecReload=/bin/kill -USR2 $MAINPID
-	ExecStop=/bin/kill -SIGINT $MAINPID
+	Type=forking
+	ExecStart=/usr/local/bin/redis-server /etc/redis/6379.conf
+	PrivateTmp=true
+
 	[Install]
 	WantedBy=multi-user.target
 	''' >> /lib/systemd/system/redis.service
-	systemctl daemon-reload
 	systemctl enable redis.service
-	systemctl start redis.service
+	systemctl restart redis.service
+
 }
+
+function menu_choice {
+	clear 
+	echo
+	echo -e "\t\t. 选择python安装的版本"
+	echo -e "\t1.  redis 4.0.6安装"
+	echo -e "\t2.  redis 6.2.3安装"
+	echo -e "\t0. Exit menu\n\n"
+	#-en 选项会去掉末尾的换行符，这让菜单看起来更专业一些
+	echo -en "\t\t Enter option:" 
+	#read 命令读取用户输入
+	read -n 1 choice_version
+	}
+while [ 1 ]
+	do 
+		menu_choice
+		case $choice_version in
+		0)
+			break ;;
+		1)
+			redis_install 4.0.6;;
+		2)
+			redis_install 6.2.3;;
+		*)
+			clear
+			echo "sorry,wrong selection" ;;
+		esac
+		echo -en "\n\n\t\thit any to contunue"
+		read -n 1 line
+done
