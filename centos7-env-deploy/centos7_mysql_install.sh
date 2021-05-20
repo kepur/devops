77d4aa925c39626c37fc0b3eaf573d91e88bc882
hozin <hozin@gmail.com>
2021-05-20 15:13:53 +0800

    add redis 修改mysql安装

--- centos7-env-deploy/centos7_mysql_install.sh
+++ centos7-env-deploy/centos7_mysql_install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo " 初始化安装请确保网络通畅DNS解析正常......" && sleep 2s
-mysql="mysql57-community-release-el7-8.noarch.rpm"
-mysqlurl="https://linux-1254084810.cos.ap-chengdu.myqcloud.com/mysql57-community-release-el7-8.noarch.rpm"
-echo "正在执行mysql安装"
-cd /opt/
-mysql574_install
 mysql574_install(){
+    cd /opt/
+    echo "正在执行mysql安装"
+    mysql="mysql57-community-release-el7-8.noarch.rpm"
+    mysqlurl="https://linux-1254084810.cos.ap-chengdu.myqcloud.com/mysql57-community-release-el7-8.noarch.rpm"
+    wget $mysqlurl
+
 	read -p "请输入mysqlroot的密码" NewPass
 	rpm -ivh mysql57-community-release-el7-8.noarch.rpm
 	yum -y install mysql-server
@@ -15,4 +16,4 @@ mysql574_install(){
 	/usr/local/mysql/bin/mysql --connect-expired-password -uroot -p${oldpass} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '"${NewPass}"';"
 	/usr/local/mysql/bin/mysql --connect-expired-password -uroot -p${oldpass} -e " flush privileges;"
 }
-
+mysql574_install
