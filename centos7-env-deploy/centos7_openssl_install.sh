55a30d03bc12dba1564ea6a3a4a88d014e9a3a62
hozin <hozin@gmail.com>
2021-06-16 10:29:32 +0800

    openssl script fix

--- centos7-env-deploy/centos7_openssl_install.sh
+++ centos7-env-deploy/centos7_openssl_install.sh
@@ -1,35 +1,34 @@
+#!/bin/bash
 
 #https://www.openssl.org/source/openssl-1.1.1k.tar.gz
 #https://www.openssl.org/source/openssl-fips-2.0.16.tar.gz
 #openssl-3.0.0-alpha17.tar.gz
-
-#!/bin/bash
+pkg_dir=/opt/pkg_dir/
+openssl_root_url="https://www.openssl.org/source/"
 echo " 初始化安装请确保网络通畅DNS解析正常......" && sleep 2s
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-options=("Option 1" "Option 2" "Option 3" "Quit")
+options=("openssl-3.0.0-alpha17" "openssl-1.1.1k" "openssl-fips-2.0.16" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "1.安装openssl-3.0.0-alpha17")
+        "openssl-3.0.0-alpha17")
             echo "you chose install openssl-3.0.0-alpha17"
             openssl_install 1.1.1k 3.0.0-alpha17
             ;;
-        "2.安装openssl-1.1.1k")
+        "openssl-1.1.1k")
             echo "you chose install openssl-1.1.1k"
 			openssl_install 1.1.1k
             ;;
-        "3.安装openssl-fips-2.0.16")
+        "openssl-fips-2.0.16")
             echo "you chose install openssl-fips-2.0.16"
 			openssl_install fips-2.0.16
             ;;
-        "4.")
+        "Quit")
             break
             ;;
         *) echo "invalid option ";;
     esac
 done
-pkg_dir=/opt/pkg_dir/
-openssl_root_url="https://www.openssl.org/source/"
 openssl_install(){
     openssl_version=$1
     echo $openssl_version
